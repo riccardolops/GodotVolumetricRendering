b45d187b21b986544d0e269ca9953dc1d7d0d982
Matias Lavik <matias.lavik@catenda.no>
2024-02-20 18:32:09 +0100

    spaces

--- addons/volumetric_importer/scripts/VolumetricRendering/gui/MovableButton.cs
+++ addons/volumetric_importer/scripts/VolumetricRendering/gui/MovableButton.cs
@@ -5,66 +5,66 @@ using System;
 [Tool]
 public partial class MovableButton : Button
 {
-	private bool isPressed = false;
-	private Vector2 originalPosition;
-	private Vector2 mousePressPosition;
-	private Vector2 minPosition;
-	private Vector2 maxPosition;
+    private bool isPressed = false;
+    private Vector2 originalPosition;
+    private Vector2 mousePressPosition;
+    private Vector2 minPosition;
+    private Vector2 maxPosition;
 
-	[Export]
-	public bool lockVerticalMovement = false;
+    [Export]
+    public bool lockVerticalMovement = false;
 
-	[Export]
-	public bool lockHorizontalMovement = false;
+    [Export]
+    public bool lockHorizontalMovement = false;
 
-	[Export]
-	public bool lockToParentRect = false;
+    [Export]
+    public bool lockToParentRect = false;
 
-	[Export(PropertyHint.Range, "0,1,")]
-	public Vector2 lockPivot = new Vector2(0.0f, 0.0f);
+    [Export(PropertyHint.Range, "0,1,")]
+    public Vector2 lockPivot = new Vector2(0.0f, 0.0f);
 
-	public Action onMove;
+    public Action onMove;
 
-	public bool IsMoving()
-	{
-		return isPressed;
-	}
+    public bool IsMoving()
+    {
+        return isPressed;
+    }
 
-	public override void _GuiInput(InputEvent @event)
-	{
-		InputEventMouseButton mouseButtonEvent = @event as InputEventMouseButton;
-		if (mouseButtonEvent != null && mouseButtonEvent.ButtonIndex == MouseButton.Left)
-		{
-			if (mouseButtonEvent.Pressed)
-			{
-				isPressed = true;
-				mousePressPosition = mouseButtonEvent.GlobalPosition;
-				originalPosition = this.GlobalPosition;
-				Rect2 rect = this.GetRect();
-				Rect2 parentRect = this.GetParentControl().GetRect();
-				minPosition = parentRect.Position - lockPivot *rect.Size;
-				maxPosition = parentRect.Position + parentRect.Size - rect.Size + lockPivot *rect.Size;
-			}
-			else
-			{
-				isPressed = false;
-				mousePressPosition = mouseButtonEvent.GlobalPosition;
-				originalPosition = this.GlobalPosition;
-			}
-		}
+    public override void _GuiInput(InputEvent @event)
+    {
+        InputEventMouseButton mouseButtonEvent = @event as InputEventMouseButton;
+        if (mouseButtonEvent != null && mouseButtonEvent.ButtonIndex == MouseButton.Left)
+        {
+            if (mouseButtonEvent.Pressed)
+            {
+                isPressed = true;
+                mousePressPosition = mouseButtonEvent.GlobalPosition;
+                originalPosition = this.GlobalPosition;
+                Rect2 rect = this.GetRect();
+                Rect2 parentRect = this.GetParentControl().GetRect();
+                minPosition = parentRect.Position - lockPivot *rect.Size;
+                maxPosition = parentRect.Position + parentRect.Size - rect.Size + lockPivot *rect.Size;
+            }
+            else
+            {
+                isPressed = false;
+                mousePressPosition = mouseButtonEvent.GlobalPosition;
+                originalPosition = this.GlobalPosition;
+            }
+        }
 
-		InputEventMouseMotion mouseMotionEvent = @event as InputEventMouseMotion;
-		if (mouseMotionEvent != null && isPressed)
-		{
-			Vector2 offset = mouseMotionEvent.GlobalPosition - mousePressPosition;
-			Vector2 newPosition = originalPosition + new Vector2(lockHorizontalMovement ? 0.0f : offset.X, lockVerticalMovement ? 0.0f : offset.Y);
-			if (lockToParentRect)
-				newPosition = newPosition.Clamp(minPosition, maxPosition);
-			this.GlobalPosition = newPosition;
-		}
-		if (!isPressed)
-		{
-			base._GuiInput(@event);
-		}
-	}
+        InputEventMouseMotion mouseMotionEvent = @event as InputEventMouseMotion;
+        if (mouseMotionEvent != null && isPressed)
+        {
+            Vector2 offset = mouseMotionEvent.GlobalPosition - mousePressPosition;
+            Vector2 newPosition = originalPosition + new Vector2(lockHorizontalMovement ? 0.0f : offset.X, lockVerticalMovement ? 0.0f : offset.Y);
+            if (lockToParentRect)
+                newPosition = newPosition.Clamp(minPosition, maxPosition);
+            this.GlobalPosition = newPosition;
+        }
+        if (!isPressed)
+        {
+            base._GuiInput(@event);
+        }
+    }
 }
